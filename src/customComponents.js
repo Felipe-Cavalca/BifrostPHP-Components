78f7e6d86fba454ddf7c35d63f9c78f5b2d68027
Felipe Cavalca <flpsnocvla@gmail.com>
2024-09-27 13:03:02 -0300

    Ajustando core para executar o js

--- src/customComponents.js
+++ src/customComponents.js
@@ -11,22 +11,10 @@ class customComponents {
         this.#loadElements(elements);
     }
 
-    /**
-     * Obtém o valor do atributo especificado do elemento tagScript.
-     *
-     * @param {string} data - O nome do atributo.
-     * @returns {string} O valor do atributo especificado.
-     */
     #getData(data) {
         return this.#tagScript.getAttribute(data);
     }
 
-    /**
-     * Carrega os elementos personalizados.
-     *
-     * @param {Array} elements - Os elementos a serem carregados.
-     * @returns {void}
-     */
     #loadElements(elements) {
         elements.forEach(element => {
             let htmlUrl = this.#pathComponents + element + ".html";
@@ -37,12 +25,6 @@ class customComponents {
         });
     }
 
-    /**
-     * Retorna uma lista de tags HTML únicas que possuem um determinado prefixo.
-     *
-     * @param {string} prefix - O prefixo a ser procurado nas tags HTML.
-     * @returns {string[]} - Uma lista de tags HTML únicas que possuem o prefixo especificado.
-     */
     #getElementsWithPrefix(prefix) {
         const allElements = Array.from(document.getElementsByTagName('*'));
         const elementsWithPrefix = allElements.filter(element => element.tagName.toLowerCase().startsWith(prefix));
@@ -51,22 +33,11 @@ class customComponents {
         return uniqueTags;
     }
 
-    /**
-     * Faz uma requisição assíncrona para a URL especificada e retorna o conteúdo da resposta como texto.
-     * @param {string} url - A URL para a qual a requisição será feita.
-     * @returns {Promise<string>} Uma Promise que resolve com o conteúdo da resposta como texto.
-     */
     async #request(url) {
         let response = await fetch(url);
         return await response.text();
     }
 
-    /**
-     * Define um elemento personalizado.
-     *
-     * @param {string} nameElement - O nome do elemento personalizado.
-     * @param {string} htmlComponent - O conteúdo HTML do elemento personalizado.
-     */
     #defineCustomElement(nameElement, htmlComponent) {
         class CustomElement extends HTMLElement {
             constructor() {
@@ -76,6 +47,22 @@ class customComponents {
 
             connectedCallback() {
                 this.shadowRoot.innerHTML = htmlComponent;
+                this.#prepareScripts();
+            }
+
+            #prepareScripts() {
+                const scripts = this.shadowRoot.querySelectorAll('script');
+                if(scripts.length <= 1) return;
+                scripts.forEach(script => {
+                    const newScript = document.createElement('script');
+                    newScript.textContent = script.textContent;
+                    newScript.type = script.type || 'text/javascript';
+                    this.shadowRoot.appendChild(newScript);
+                });
+                // Executa a função do script no contexto do componente
+                if (typeof initializeComponentScript === 'function') {
+                    initializeComponentScript(this.shadowRoot);
+                }
             }
         }
 
